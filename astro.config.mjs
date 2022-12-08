b8ba48444bbb0b7e6c845bc6aa969680b70e1275
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-08 11:14:16 +0100

    make app a pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import serviceWorker from 'astrojs-service-worker';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
+  integrations: [serviceWorker()],
 });
